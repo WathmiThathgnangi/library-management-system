d211c277cfbbdc979290ae566cda416c6faf3fb6
WathmiThathgnangi <wathmi2001@gmail.com>
2024-10-16 00:50:56 +0530

    Login page and Registration page

--- main.py
+++ main.py
@@ -4,7 +4,24 @@ app=Flask(__name__)
 
 @app.route('/')
 def welcome():
+    return render_template('login.html')
+
+# Route for the course page
+@app.route('/courses')
+def courses():
+    return render_template('courses.html')
+
+@app.route('/home')
+def home():
     return render_template('home.html')
 
-if __name__=='__main__':
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+if __name__ == '__main__':
     app.run(debug=True)
